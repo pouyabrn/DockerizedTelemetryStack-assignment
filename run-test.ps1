3308c9e8187323126bd4ed121ccda3b537faabe0
pouee <122753653+pouyabrn@users.noreply.github.com>
2025-10-15 23:04:10 +0200

    Delete run-test.ps1

--- run-test.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Starting Car Telemetry Test Generator..." -ForegroundColor Green
-Write-Host "Press Ctrl+C to stop" -ForegroundColor Yellow
-Write-Host ""
-
-Set-Location "C:\Users\pouee\Docker task\DockerizedTelemetryStack-assignment"
-wsl bash -c "cd '/mnt/c/Users/pouee/Docker task/DockerizedTelemetryStack-assignment' && node scripts/test-telemetry.js"
-

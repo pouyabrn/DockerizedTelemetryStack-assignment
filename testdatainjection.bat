c935ab91bf2a759d26c2372d5a3591dfdeedf066
pouee <122753653+pouyabrn@users.noreply.github.com>
2025-10-15 23:04:32 +0200

    Delete testdatainjection.bat

--- testdatainjection.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Car Telemetry Test Generator...
-echo Press Ctrl+C to stop
-echo.
-wsl bash -c "cd '/mnt/c/Users/pouee/Docker task/DockerizedTelemetryStack-assignment' && node scripts/test-telemetry.js"
-
